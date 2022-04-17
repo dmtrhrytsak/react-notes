ae6535fee674af851fa9f7d7285f736552f357bf
Dmitriy Hrytsak <hrytsakmail@gmail.com>
2022-04-17 16:57:24 +0300

    Fix overflow table issue

--- src/views/archive/ArchiveNotesTable.tsx
+++ src/views/archive/ArchiveNotesTable.tsx
@@ -15,48 +15,50 @@ const ArchiveNotesTable: React.FC<ArchiveNotesTableProps> = ({
   onNoteUnarchive,
 }) => {
   return (
-    <Table>
-      <Table.Head>
-        <Table.Row>
-          {headCells.map((headCell) => (
-            <Table.HeadCell key={headCell}>{headCell}</Table.HeadCell>
-          ))}
-        </Table.Row>
-      </Table.Head>
+    <Table.Wrapper>
+      <Table>
+        <Table.Head>
+          <Table.Row>
+            {headCells.map((headCell) => (
+              <Table.HeadCell key={headCell}>{headCell}</Table.HeadCell>
+            ))}
+          </Table.Row>
+        </Table.Head>
 
-      <Table.Body>
-        {notes.map((note) => (
-          <Table.Row key={note.id}>
-            <Table.RowCell>
-              <div className="flex items-center">
-                <Icon type={note.category} />
+        <Table.Body>
+          {notes.map((note) => (
+            <Table.Row key={note.id}>
+              <Table.RowCell>
+                <div className="flex items-center">
+                  <Icon type={note.category} />
 
-                <div>
-                  <p>{note.name}</p>
+                  <div>
+                    <p>{note.name}</p>
 
-                  <div className="flex space-x-1 text-sm text-blue-500">
-                    <button
-                      className="underline"
-                      onClick={() => onNoteUnarchive(note.id)}
-                    >
-                      Unarchive
-                    </button>
+                    <div className="flex space-x-1 text-sm text-blue-500">
+                      <button
+                        className="underline"
+                        onClick={() => onNoteUnarchive(note.id)}
+                      >
+                        Unarchive
+                      </button>
+                    </div>
                   </div>
                 </div>
-              </div>
-            </Table.RowCell>
-            <Table.RowCell variant="secondary">
-              {formatDate(note.createdAt)}
-            </Table.RowCell>
-            <Table.RowCell variant="secondary">
-              {capitalize(note.category)}
-            </Table.RowCell>
-            <Table.RowCell variant="secondary">{note.content}</Table.RowCell>
-            <Table.RowCell variant="secondary">{note.dates}</Table.RowCell>
-          </Table.Row>
-        ))}
-      </Table.Body>
-    </Table>
+              </Table.RowCell>
+              <Table.RowCell variant="secondary">
+                {formatDate(note.createdAt)}
+              </Table.RowCell>
+              <Table.RowCell variant="secondary">
+                {capitalize(note.category)}
+              </Table.RowCell>
+              <Table.RowCell variant="secondary">{note.content}</Table.RowCell>
+              <Table.RowCell variant="secondary">{note.dates}</Table.RowCell>
+            </Table.Row>
+          ))}
+        </Table.Body>
+      </Table>
+    </Table.Wrapper>
   );
 };
 
